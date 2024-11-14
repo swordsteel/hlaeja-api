024de40aae55edcbfde9ce56d0b157a88a55a51f
Swordsteel <swordsteel@gmail.com>
2024-11-14 13:05:18 +0100

    add device data property

--- src/main/kotlin/ltd/hlaeja/Application.kt
+++ src/main/kotlin/ltd/hlaeja/Application.kt
@@ -1,8 +1,11 @@
 package ltd.hlaeja
 
+import ltd.hlaeja.property.DeviceDataProperty
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
+@EnableConfigurationProperties(DeviceDataProperty::class)
 @SpringBootApplication
 class Application
 
