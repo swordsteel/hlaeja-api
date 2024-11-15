0bdf0449b833777f6c1927cb54a47c06ff47265f
Swordsteel <swordsteel@gmail.com>
2024-11-15 20:40:25 +0100

    add addUnitMeasurement endpoint to MeasurementController

--- src/main/kotlin/ltd/hlaeja/controller/MeasurementController.kt
+++ src/main/kotlin/ltd/hlaeja/controller/MeasurementController.kt
@@ -1,11 +1,16 @@
 package ltd.hlaeja.controller
 
 import java.util.UUID
+import ltd.hlaeja.library.deviceData.MeasurementData
 import ltd.hlaeja.service.DeviceDataService
 import ltd.hlaeja.service.DeviceRegistryService
+import org.springframework.http.HttpStatus.CREATED
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -20,4 +25,25 @@ class MeasurementController(
         @PathVariable unit: UUID,
     ): Map<String, Number> = deviceRegistry.getClientDevice(unit)
         .let { (client, device) -> deviceDataService.getMeasurement(client, device).fields }
+
+    @PostMapping("/unit-{unit}")
+    @ResponseStatus(CREATED)
+    suspend fun addUnitMeasurement(
+        @PathVariable unit: UUID,
+        @RequestBody measurement: Map<String, Number>,
+    ) {
+        deviceRegistry.getClientDevice(unit)
+            .let { (client, device) ->
+                deviceDataService.addMeasurement(
+                    client,
+                    MeasurementData.Request(
+                        mutableMapOf(
+                            "unit" to unit.toString(),
+                            "device" to device.toString(),
+                        ),
+                        measurement,
+                    ),
+                )
+            }
+    }
 }
